5535f040790a7fecede5d54ac895108506342503
Charlie <hammersleycharlie@gmail.com>
2025-09-09 00:31:39 +0000

    Update meet-the-team.tsx
    Updated some of the text for the About Us section.

--- src/react/pages/meet-the-team.tsx
+++ src/react/pages/meet-the-team.tsx
@@ -23,13 +23,9 @@ export class MeetTheTeam extends React.Component<Props, State> {
 		{
 			content: (
 				<p>
-					Team 1711 The RAPTORS was formed in 2007 initially by a teacher and a small group of students eager
-					to learn about FIRST Robotics. That teacher, Mr. John Failor, continues to connect us with Traverse
-					City Area Public Schools (TCAPS) and works with the team. Meanwhile, our head mentor, Mrs. Jennifer
-					DeZeeuw, has been on the team for six years. She organizes our events, keeps them fun, and ensures
-					that meetings run smoothly. Our team meets multiple times per week, beginning in the fall and
-					accelerating into build. Some of our traditions include team dances and Wednesday night meals
-					together.
+					Team 1711 The RAPTORS was formed in 2007 initially by Mr. John Failor and a small group of students eager
+					to learn about FIRST Robotics. Our team meets multiple times per week year round, ramping up practices into 
+					the build season.
 				</p>
 			),
 			image: aboutUsImg1,
@@ -37,8 +33,7 @@ export class MeetTheTeam extends React.Component<Props, State> {
 		},
 		{
 			content: <p>
-				RAPTORS stands for Robots and People Transmitting Objective Radical Synergy, and our mascot is
-				Lawrence, the Raptor.
+				RAPTORS stands for Robots and People Transmitting Objective Radical Synergy.
 			</p>,
 			image: aboutUsImg2,
 			imageAlt: "The RAPTORS' mascot being waved during a competition."
@@ -47,17 +42,17 @@ export class MeetTheTeam extends React.Component<Props, State> {
 			content: <p>
 				Team 1711 is heavily involved in our community, especially through connecting with younger students.
 				Our biggest annual event is Super Science Saturday, which encourages elementary school students to
-				develop an interest in STEM through hands-on activities and guest speakers. Mrs. DeZeeuw spearheads
-				the logistics for this event each year, another example of her countless contributions to the team.
+				develop an interest in STEM through hands-on activities and guest speakers.
 			</p>,
 			image: aboutUsImg3,
 			imageAlt: "A number of RAPTORS team members running a booth at the Super Science Saturday event."
 		},
 		{
 			content: <p>
-				The RAPTORS love to have fun in and out of practice, but we have had many accomplishments throughout
-				the years, most recently winning Chairman’s and advancing to States in the 2021 season. We are
-				excited to return to the field this year and hope you will follow our progress.
+				The RAPTORS love to have fun in and out of practice, but we have had many accomplishments 
+				throughout the years, most recently winning three awards total, one at each event, in the 
+				2025 competition season. We are excited to return to the field this year and hope you will 
+				follow our progress.
 			</p>,
 			image: aboutUsImg4,
 			imageAlt: "A number of RAPTORS team members cheering from the stands during an FRC competition."
